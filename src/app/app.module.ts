4001f3bf7dab4fc22dcc1c9783058ebc0787597b
Famouse <famouse_ru@mail.ru>
2022-10-15 09:40:15 +0300

    chore: init map module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HeaderComponent} from "./shared";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
-import {MapComponent} from "./map/map.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MapComponent
   ],
   imports: [
     BrowserModule,
