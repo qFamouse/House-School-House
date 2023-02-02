c70f37be78a3b12d34760a1fec895dc180304ab0
Famouse <famouse_ru@mail.ru>
2023-02-02 15:49:14 +0300

    chore: complete layout system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import {AppRoutingModule} from "./app-routing.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LayoutModule } from "./layout/layout.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatIconModule,
-    MatButtonModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		LayoutModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./components/header/header.component";
+import { LayoutComponent } from "./components/layout/layout.component";
+import { LayoutRoutingModule } from "./layout-routing.module";
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+	declarations: [HeaderComponent, LayoutComponent],
+	imports: [CommonModule, LayoutRoutingModule],
+	bootstrap: [LayoutComponent]
 })
-export class LayoutModule { }
+export class LayoutModule {}
