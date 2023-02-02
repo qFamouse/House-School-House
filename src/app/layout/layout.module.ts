490a46166bd71e3d518a2c4b9bcf49f95b8ee064
Famouse <famouse_ru@mail.ru>
2023-02-02 16:41:30 +0300

    chore: complete layout header

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from "@angular/common";
 import { LayoutHeaderComponent } from "./components/header/layout-header.component";
 import { LayoutComponent } from "./components/layout/layout.component";
 import { LayoutRoutingModule } from "./layout-routing.module";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
 	declarations: [LayoutHeaderComponent, LayoutComponent],
-	imports: [CommonModule, LayoutRoutingModule],
+	imports: [CommonModule, LayoutRoutingModule, MatIconModule, MatButtonModule],
 	bootstrap: [LayoutComponent]
 })
 export class LayoutModule {}
