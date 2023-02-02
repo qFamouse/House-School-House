c34a3c79add57c392387151bf9ab97e946ffe174
Famouse <famouse_ru@mail.ru>
2023-02-02 16:45:05 +0300

    chore: adding map component, configure route to this component

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LayoutComponent } from "./components/layout/layout.component";
+import { MapPageComponent } from "../modules/map/components/map-page/map-page.component";
 
 const routes: Routes = [
 	{
 		path: "",
-		component: LayoutComponent
+		component: LayoutComponent,
+		children: [
+			{
+				path: "map",
+				component: MapPageComponent
+			}
+		]
 	}
 ];
 
---
+++ src/app/modules/map/map.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapPageComponent } from './components/map-page/map-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    MapPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MapModule { }
