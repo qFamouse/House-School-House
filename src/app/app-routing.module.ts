72aa803dbc28e54db188d06c63cf67d5494f6ef9
Famouse <famouse_ru@mail.ru>
2023-02-02 13:56:20 +0300

    chore: clear project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    // I don't know what's better to import MODULE or COMPONENT. Because for component we should import LeafletModule into app-module, I prefer load map module
-    path: '',
-    loadChildren: () => import('./modules/map/map.module').then(m => m.MapModule)
-  }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HeaderComponent} from "./shared";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
