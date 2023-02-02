deaef6fe0e22047c0b574e45d25a456471ce8512
Famouse <famouse_ru@mail.ru>
2023-02-02 14:57:07 +0300

    chore: adding prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+// The values provided below are the defaults.
+// If you don't specify one of these properties,
+// the default value will be applied.
+module.exports = {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: false,
+	trailingComma: "none",
+	bracketSpacing: true,
+	bracketSameLine: true,
+	arrowParens: "avoid",
+	parser: "typescript"
+};
