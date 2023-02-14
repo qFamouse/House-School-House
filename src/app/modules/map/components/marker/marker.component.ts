5f511594dcd75f939a4e04f35ab4cc3677aa229e
Famouse <famouse_ru@mail.ru>
2023-02-15 00:33:21 +0300

    chore: create marker component with simple styles

---
+++ src/app/modules/map/components/marker/marker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marker',
+  templateUrl: './marker.component.html',
+  styleUrls: ['./marker.component.scss']
+})
+export class MarkerComponent {
+
+}
--- src/app/modules/map/map.module.ts
+++ src/app/modules/map/map.module.ts
@@ -4,9 +4,10 @@ import { MapPageComponent } from "./components/map-page/map-page.component";
 import { LeafletModule } from "@asymmetrik/ngx-leaflet";
 import { MatIconModule } from "@angular/material/icon";
 import { HttpClientModule } from "@angular/common/http";
+import { MarkerComponent } from "./components/marker/marker.component";
 
 @NgModule({
-	declarations: [MapPageComponent],
+	declarations: [MapPageComponent, MarkerComponent],
 	imports: [CommonModule, LeafletModule, MatIconModule, HttpClientModule]
 })
 export class MapModule {}
