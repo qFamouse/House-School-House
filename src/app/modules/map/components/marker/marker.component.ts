fce232e8978e452f44f5ad5b7829087ccace0d38
Famouse <famouse_ru@mail.ru>
2023-02-15 22:20:10 +0300

    chore: update marker component

--- src/app/modules/map/components/marker/marker.component.ts
+++ src/app/modules/map/components/marker/marker.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-marker',
-  templateUrl: './marker.component.html',
-  styleUrls: ['./marker.component.scss']
+	selector: "app-marker",
+	templateUrl: "./marker.component.html",
+	styleUrls: ["./marker.component.scss"]
 })
 export class MarkerComponent {
-
+	@Input() src: string;
+	@Input() alt: string = "Маркер";
+	@Input() title: string;
+	@Input() description: string;
 }
