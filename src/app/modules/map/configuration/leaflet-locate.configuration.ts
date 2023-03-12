b468d1361d1725a5108dfa33c2d75cf0d42a60dc
Famouse <famouse_ru@mail.ru>
2023-03-12 13:38:26 +0300

    chore: return draw circle to true

--- src/app/modules/map/configuration/leaflet-locate.configuration.ts
+++ src/app/modules/map/configuration/leaflet-locate.configuration.ts
@@ -2,7 +2,7 @@ import { Control } from "leaflet";
 import "leaflet.locatecontrol";
 
 export let locateControl = new Control.Locate({
-	drawCircle: false,
+	drawCircle: true,
 	initialZoomLevel: 16,
 	strings: {
 		title: "Моё местоположение",
