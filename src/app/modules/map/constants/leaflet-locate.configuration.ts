76933e3b8be29bffe3b1a14e2e88b75d00e4a53e
Famouse <famouse_ru@mail.ru>
2023-04-06 01:45:50 +0300

    chore: correct disabling popup menu

--- src/app/modules/map/constants/leaflet-locate.configuration.ts
+++ src/app/modules/map/constants/leaflet-locate.configuration.ts
@@ -4,8 +4,8 @@ import "leaflet.locatecontrol";
 export let locateControl = new Control.Locate({
 	drawCircle: true,
 	initialZoomLevel: 16,
+	showPopup: false,
 	strings: {
-		title: "Моё местоположение",
-		popup: undefined
+		title: "Моё местоположение"
 	}
 });
